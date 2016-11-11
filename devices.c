781ad3a85f0f533eae7f8102b5707c6e30601ae3
azarenko <azarenko.s@gmail.com>
2016-11-11 13:57:04 +0200

    fix double free result

--- devices.c
+++ devices.c
@@ -128,7 +128,7 @@ PGresult *getexecsql(PGconn *conn, char * sql)
     if (!res || PQresultStatus(res) != PGRES_COMMAND_OK){ 
             if(debug)syslog(LOG_ERR,"BEGIN failed");
             PQclear(res);
-            return res;
+            return NULL;
     }
     PQclear(res);
     if(debug>1)syslog(LOG_ERR,"BEGIN ok");
@@ -149,7 +149,7 @@ PGresult *getexecsql(PGconn *conn, char * sql)
     if (!res || PQresultStatus(res) != PGRES_COMMAND_OK){
         if(debug)syslog(LOG_ERR,"DECLARE failed");
         PQclear(res);
-        return res;
+        return NULL;
     }
     PQclear(res);
     free(sqlfull);
@@ -162,7 +162,7 @@ PGresult *getexecsql(PGconn *conn, char * sql)
     if (!res || PQresultStatus(res) != PGRES_TUPLES_OK){
         if(debug)syslog(LOG_ERR,"FETCH failed");
         PQclear(res);
-        return res;
+        return NULL;
     }
     if(debug>1)syslog(LOG_ERR,"FETCH ok");
     return res;
@@ -174,12 +174,16 @@ void clearres(PGconn *conn, PGresult *res)
     {
         return;
     }
-    
+
+    if(res)
+    {
+    	PQclear(res);
+    }   
+ 
     char request[MAXLENQUERY];
     bzero(request, MAXLENQUERY);    
     sprintf(request, CLOSE, conn);
-    
-    PQclear(res);
+   
     res = PQexec(conn, request);
     if(debug>1)syslog(LOG_ERR,"CLOSE ok");
     PQclear(res);
